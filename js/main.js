b57d04d33ade4353cbd8942ab198f1389aee56c4
Brenton Partridge <bapartridge@gmail.com>
2012-11-14 23:19:59 -0500

    Touch to scroll collection table

--- js/main.js
+++ js/main.js
@@ -1,3 +1,9 @@
-forge.logging.info("test");
+(function() {
+  forge.logging.info("test");
 
-
+  var init = function() {
+    forge.logging.info("loaded");
+    var scroll = new iScroll("collection-table-container")
+  };
+  $(init);
+})();
